514588494bae5398ec6399edcc7c2b1a286f7fd8
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-07 20:01:05 +0100

    initial index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+
+// let hello = "Hallo React";
+
+function App() {
+  return <h1>Wilkommen zu React</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
