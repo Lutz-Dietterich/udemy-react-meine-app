6bde87ef1bd8942845e38b318cefbbc7816963ab
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-07 20:07:22 +0100

    initial App.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import ReactDOM from "react-dom";
+import App from "./App";
 
 // let hello = "Hallo React";
-
-function App() {
-  return <h1>Wilkommen zu React</h1>;
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
