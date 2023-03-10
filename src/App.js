53b6b5f48db76eed0b65ba4cca3053a165928ff3
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-10 21:41:02 +0100

    state and buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "./index.css";
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <h1>Wilkommen zu React</h1>;
+  let name = "React";
+  const [zahl, setZahl] = useState(1);
+  const [karte, setKarte] = useState(false);
+
+  return (
+    <div className="center container paper">
+      <Header name={name} />
+      <div className="row">
+        <button
+          onClick={() => setZahl(zahl + 1)}
+          className="btn-success-outline"
+        >
+          +1
+        </button>
+        <button
+          onClick={() => setZahl(zahl - 1)}
+          className="btn-success-outline"
+        >
+          -1
+        </button>
+        <button onClick={() => setKarte(!karte)} className="btn-danger-outline">
+          Karte
+        </button>
+      </div>
+      <Main nummer={zahl} inhalt="Guybrush" karte={karte} />
+      <Main nummer="2" inhalt="LeChuck" karte={karte} />
+      <Main karte={karte} />
+    </div>
+  );
 }
 
 export default App;
